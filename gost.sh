3a96fc50225bbd38faf636b35e1b0dba91468597
qqrrooty <64602472+qqrrooty@users.noreply.github.com>
2024-10-07 09:04:42 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -103,7 +103,7 @@ function Install_ct() {
     mv gost /usr/bin/gost
     chmod -R 777 /usr/bin/gost
     wget --no-check-certificate https://ghp.ci/raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system
-    mkdir /etc/gost && wget --no-check-certificate https://gotunnel.oss-cn-shenzhen.aliyuncs.com/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
+    mkdir /etc/gost && wget --no-check-certificate https://ghp.ci/raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   else
     rm -rf gost-linux-"$bit"-"$ct_new_ver".gz
     wget --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz
