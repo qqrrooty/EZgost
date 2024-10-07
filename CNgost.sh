d12959c5aeb3d598c4a9d07fbbcc32d7a3d1711c
qqrrooty <64602472+qqrrooty@users.noreply.github.com>
2024-10-07 09:24:58 +0800

    Update CNgost.sh

--- CNgost.sh
+++ CNgost.sh
@@ -872,14 +872,14 @@ cron_restart() {
 }
 
 update_sh() {
-  ol_version=$(curl -L -s --connect-timeout 5 https://ghp.ci/raw.githubusercontent.com/qqrrooty/EZgost/master/gost.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
+  ol_version=$(curl -L -s --connect-timeout 5 https://ghp.ci/raw.githubusercontent.com/qqrrooty/EZgost/master/CNgost.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
   if [ -n "$ol_version" ]; then
     if [[ "$shell_version" != "$ol_version" ]]; then
       echo -e "存在新版本，是否更新 [Y/N]?"
       read -r update_confirm
       case $update_confirm in
       [yY][eE][sS] | [yY])
-        wget -N --no-check-certificate https://ghp.ci/raw.githubusercontent.com/qqrrooty/EZgost/master/gost.sh
+        wget -N --no-check-certificate https://ghp.ci/raw.githubusercontent.com/qqrrooty/EZgost/master/CNgost.sh
         echo -e "更新完成"
         exit 0
         ;;
